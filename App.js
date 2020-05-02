f6a22d8c1421f72495d5e8c39a79ef6fdd371a34
Daniel Warren <warrend555@gmail.com>
2020-05-02 19:59:40 -0400

    init

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ styles/colors.js
@@ -0,0 +1,34 @@
+const green = '#359D89';
+const white = '#ffffff';
+const black = '#222222';
+
+const borderRadius = 10;
+// const boxShadow = '';
+
+export const styles = {
+  backgroundColor: {
+    primary: green,
+    button: green,
+    progress: white,
+    page: white,
+  },
+  fontColor: {
+    primary: black,
+    mainHeader: black,
+    secondaryHeader: green,
+    header: green,
+    progress: green,
+    page: black,
+    button: white,
+    completedHeader: white,
+    textButton: green,
+  },
+  item: {
+    gear: white,
+    check: green,
+    underline: green,
+  },
+  preFabs: {
+    borderRadius: borderRadius,
+  }
+};
